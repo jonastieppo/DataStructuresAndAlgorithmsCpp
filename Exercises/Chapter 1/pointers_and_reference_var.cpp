03ac562bcbf810a12523f25562634dc4744fb87e
Jonas Tieppo da Rocha <jonas.rocha@ska.com.br>
2022-07-27 08:55:59 -0300

    Passing Arguments By Reference

--- Exercises/Chapter 1/pointers_and_reference_var.cpp
+++ Exercises/Chapter 1/pointers_and_reference_var.cpp
@@ -4,12 +4,26 @@ using namespace std;
 
 class PointersAndRefVar {
 public:
-	 PointersAndRefVar() {
+	PointersAndRefVar() {
 		int n = 0, * p = &n, & r = n;
 		// &r is a reference var. It is implemented as a constant pointer to a variable. 
 		cout << "\n =================== pointers and reference var ================== \n";
 		for (n; n < 10; n++) {
 			cout << "\n\t values (n | *p | &r) " << n << ' ' << *p << ' ' << r << endl;
 		}
+
+		int i = 10, j = 20 , k = 40;
+		cout << "\n\n\t---------------------------------";
+		cout << "\n Vars before PassingArgumentsByReference (): (i,j,k) " << "i: " << i << " j: " << j << " k: " << k << endl;
+		PassingArgumentsByReference(i, &j, k);
+		cout << "\n Vars after PassingArgumentsByReference (): (i,j,k) " << "i: " << i << " j: " << j << " k: " << k << endl;
 	}
+	void PassingArgumentsByReference(int i, int* j, int& k) {
+		i = 2; //won't change in function call
+		*j = 4; // change in function call
+		k = 10; //change in function call
+
+	}
+
+
 };
