1560c5ac17c82740da1abbe64cf2cea23816ea77
Jonas Tieppo da Rocha <jonas.rocha@ska.com.br>
2022-07-27 10:20:32 -0300

    Pointer as return type

--- Exercises/Chapter 1/pointers_and_reference_var.cpp
+++ Exercises/Chapter 1/pointers_and_reference_var.cpp
@@ -17,13 +17,39 @@ public:
 		cout << "\n Vars before PassingArgumentsByReference (): (i,j,k) " << "i: " << i << " j: " << j << " k: " << k << endl;
 		PassingArgumentsByReference(i, &j, k);
 		cout << "\n Vars after PassingArgumentsByReference (): (i,j,k) " << "i: " << i << " j: " << j << " k: " << k << endl;
+
+		int a[] = { 1,2,3,4,5 };
+
+		int return_reference = functionReferenceType(a, 3);
+		cout << "\n-------------- Reference as return type for functions ----------------------\n";
+		cout << "\n\t\t int return_reference = functionReferenceType(a, 3) \n\t\t return_reference = " << return_reference << endl;
+		functionReferenceType(a, 3) = 40;
+		cout << "\n\t\t functionReferenceType(a, 3) = 40 \n\t\t a[3] = " << a[3] << endl;
+		cout << "\n----------------------------------------------------------------------------\n";
+
+		int pointer_return_type = *functionDeference(a, 3);
+		cout << "\n-------------- Pointer as return type for functions ----------------------\n";
+		cout << "\n\t\t int pointer_return_type = *functionDeference(a, 3) \n\t\t pointer_return_type = " << pointer_return_type << endl;
+		*functionDeference(a, 3) = 60;
+		cout << "\n\t\t *functionDeference(a, 3) = 60 \n\t\t a[3] = " << a[3] << endl;
+		cout << "\n----------------------------------------------------------------------------\n";
+
 	}
 	void PassingArgumentsByReference(int i, int* j, int& k) {
 		i = 2; //won't change in function call
 		*j = 4; // change in function call
 		k = 10; //change in function call
-
 	}
+
+	int& functionReferenceType(int a[], int i) {
+		return a[i];
+	}
+
+	int* functionDeference(int a[], int i) {
+		return &a[i];
+	}
+
+
 
 
 };
